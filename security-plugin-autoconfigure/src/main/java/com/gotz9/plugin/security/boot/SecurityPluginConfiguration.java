cf2164a14cc79efd3bfef794d107f76132fac9fc
xh-gotz9 <xh353127981@gmail.com>
2021-07-07 11:47:42 +0800

    SecurityPlugin 默认启用 Method 鉴权

--- security-plugin-autoconfigure/src/main/java/com/gotz9/plugin/security/boot/SecurityPluginConfiguration.java
+++ security-plugin-autoconfigure/src/main/java/com/gotz9/plugin/security/boot/SecurityPluginConfiguration.java
@@ -1,11 +1,15 @@
 package com.gotz9.plugin.security.boot;
 
+import com.gotz9.plugin.security.boot.method.MethodSecurityPluginConfiguration;
+import com.gotz9.plugin.security.boot.method.MethodSecurityPluginProperties;
 import com.gotz9.plugin.security.boot.web.WebSecurityPluginConfigurerAdapter;
 import com.gotz9.plugin.security.boot.web.WebSecurityPluginProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
@@ -19,6 +23,15 @@ public class SecurityPluginConfiguration {
     @EnableConfigurationProperties(WebSecurityPluginProperties.class)
     @EnableWebSecurity
     static class DefaultWebSecurityPluginConfiguration extends WebSecurityPluginConfigurerAdapter {
+
+    }
+
+    @Configuration
+    @ConditionalOnClass(GlobalMethodSecurityConfiguration.class)
+    @ConditionalOnMissingBean(GlobalMethodSecurityConfiguration.class)
+    @EnableConfigurationProperties(MethodSecurityPluginProperties.class)
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    static class DefaultMethodSecurityPluginConfiguration extends MethodSecurityPluginConfiguration {
 
     }
 
