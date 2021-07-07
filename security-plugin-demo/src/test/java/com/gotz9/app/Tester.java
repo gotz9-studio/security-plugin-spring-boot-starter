e01910f1db8918e7b6b9928852d8a0b55a3531be
xh-gotz9 <xh353127981@gmail.com>
2021-07-07 11:51:31 +0800

    增加 AntMatcher 测试

--- security-plugin-demo/src/test/java/com/gotz9/app/Tester.java
+++ security-plugin-demo/src/test/java/com/gotz9/app/Tester.java
@@ -1,14 +1,23 @@
 package com.gotz9.app;
 
 import org.aopalliance.intercept.MethodInvocation;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.aop.framework.ReflectiveMethodInvocation;
+import org.springframework.util.AntPathMatcher;
 
 public class Tester {
 
     @Test
     public void test() {
         System.out.println(MethodInvocation.class.isAssignableFrom(ReflectiveMethodInvocation.class));
+    }
+
+    @Test
+    public void antMatcherTest() {
+        AntPathMatcher matcher = new AntPathMatcher(":");
+
+        Assert.assertFalse(matcher.match("test:data", "test:data:op"));
     }
 
 }
