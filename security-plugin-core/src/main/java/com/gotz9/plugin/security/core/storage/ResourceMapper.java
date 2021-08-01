1e7198e1ee7ef2aca5f6696bde44fdde3ccbb375
gotZ9 <xh353127981@gmail.com>
2021-08-02 02:04:33 +0800

    core 引入 mybatis, 添加资源相关数据库操作

---
+++ security-plugin-core/src/main/java/com/gotz9/plugin/security/core/storage/ResourceMapper.java
@@ -0,0 +1,22 @@
+package com.gotz9.plugin.security.core.storage;
+
+import com.gotz9.plugin.security.core.subject.SysMenu;
+import com.gotz9.plugin.security.core.subject.SysPermission;
+import com.gotz9.plugin.security.core.subject.SysRole;
+import com.gotz9.plugin.security.core.subject.SysUser;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Set;
+
+@Mapper
+public interface ResourceMapper {
+
+    SysUser selectUserByUsername(String username);
+
+    Set<SysRole> loadRoleByUserId(long id);
+
+    Set<SysPermission> loadPermissionByUserId(long id);
+
+    Set<SysMenu> loadMenuByUserId(long id);
+
+}
