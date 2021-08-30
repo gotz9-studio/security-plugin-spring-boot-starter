299b584f238bbe0a52521975abb533e6286c85c0
gotZ9 <xh353127981@gmail.com>
2021-08-31 02:18:30 +0800

    新增 security-plugin-storage 模块
    - 移动 ResourceMapper 和 DatabaseUserDetailsService
    - autoconfigure 添加依赖

--- security-plugin-storage/src/main/java/com/gotz9/plugin/security/core/auth/DatabaseUserDetailsService.java
+++ security-plugin-storage/src/main/java/com/gotz9/plugin/security/core/auth/DatabaseUserDetailsService.java
@@ -9,13 +9,11 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Component
 public class DatabaseUserDetailsService implements UserDetailsService {
 
     private final ResourceMapper resourceMapper;
